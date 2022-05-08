d0c24bbed7a5e12f7e563a3ee81a29324b957f47
elielaurentraphael <raphelaur@gmail.com>
2022-05-08 21:25:06 +0300

    Commit N°07 - (à suivre)

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(window).scroll(function() {
         let scrolli = $(window).scrollTop();
         console.log(scrolli);
-        if (scrolli < 3900) {
+        if (scrolli < 4200) {
             $(".scroi").css("margin-top", scrolli + "px");
             console.log(scrolli);
         }
